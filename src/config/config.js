02bb7be089ed702419b054782c88e6c31347597d
Luiza <luizagsm@gmail.com>
2021-07-17 09:47:09 -0300

    muda tudo

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,22 @@
 //faz a comunicação com os servidor
+require('dotenv').config(); // this is important!
+
 module.exports = {
-    development:{
-        database:{
-            host:'localhost',
-            port:3306,
-            name:'global',
-            dialect: 'mysql',
-            user: 'root',
-            password:''
+    development: {
+        database: {
+            host: process.env.DB_HOST_URL,
+            port: process.env.DB_PORT,
+            name: process.env.DB_NAME,
+            dialect: 'postgres',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+
         }
     },
-    production:{
-        database:{
-            host:process.env.DB_HOST,
-            host:process.env.DB_PORT
+    production: {
+        database: {
+            host: process.env.DATABASE_URL,
+            host: process.env.DB_PORT,
         }
     }
 }
-
--- src/database/database.js
+++ src/database/database.js
@@ -1,31 +1,22 @@
-//iniciando o sequelize
 const Sequelize = require('sequelize');
+const config = require('../config/config');
 
-//usando modo dev
-const environment = process.env.NODE_ENV || 'development';
 
-//cria um objeto que recebera os dados de development
-const config = require('../config/config.js')[environment];
-//é como se o config recebesse as informações do modules dentro do arquivo config
-// config = {
-//  database:{
-//     host:'localhost',
-//     port:3306,
-//     name:'global',
-//     dialec:'mysql',
-//     user:'root',
-//     password:''
-//  }
 
-//criando um obejto chamado sequelize que faz uso da classe sequelize e passa as informaçoes do banco de dados que estao dentro do config, pra ele ter o caminho do banco de dados
-const sequelize = new Sequelize (
-    config.database.name, //servicos
-    config.database.user, //root
-    config.database.password, //vazio
-    {
-        host: config.database.host, //localhost
-        dialect: config.database.dialect //mysql
-    }
-)
-    
+const sequelize = new Sequelize({
+    database: config.development.database.name,
+    username: config.development.database.user,
+    password: config.development.database.password,
+    host: config.development.database.host,
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+        }
+    },
+});
+
+
 module.exports = sequelize;
